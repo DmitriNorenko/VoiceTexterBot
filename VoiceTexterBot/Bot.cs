ff3ddfc4aacd0d26b801e01feae1e367e6725882
Name <email@exaple.ru>
2023-08-03 11:01:50 +0300

    Обновление класса Bot.

--- VoiceTexterBot/Bot.cs
+++ VoiceTexterBot/Bot.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
 using Telegram.Bot;
 using Telegram.Bot.Exceptions;
 using Telegram.Bot.Polling;
@@ -12,28 +13,36 @@ using Telegram.Bot.Types.Enums;
 
 namespace VoiceTexterBot
 {
-    internal class Bot
+    internal class Bot : BackgroundService
     {
         private ITelegramBotClient _telegramClient;
         public Bot(ITelegramBotClient telegramClient)
         {
             _telegramClient = telegramClient;
         }
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _telegramClient.StartReceiving(
+                HandleUpdateAsync,
+                HandleErrorAsync,
+                new ReceiverOptions() { AllowedUpdates = { } },
+                cancellationToken: stoppingToken);
+        }
         async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update,
-            CancellationToken cansellationToken)
+            CancellationToken cancellationToken)
         {
             if (update.Type == UpdateType.CallbackQuery)
             {
                 await
                     _telegramClient.SendTextMessageAsync(update.Message.Chat.Id, "Вы нажали кнопку",
-                    cancellationToken: cansellationToken);
+                    cancellationToken: cancellationToken);
                 return;
             }
             if (update.Type == UpdateType.Message)
             {
                 await
                     _telegramClient.SendTextMessageAsync(update.Message.Chat.Id,
-                    "Вы отправили сообщение", cancellationToken: cansellationToken);
+                    "Вы отправили сообщение", cancellationToken: cancellationToken);
                 return;
             }
         }
