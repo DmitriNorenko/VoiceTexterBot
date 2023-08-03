aa3e9ce7938accb207c7ff51d18e689081a5339c
Name <email@exaple.ru>
2023-08-03 10:33:29 +0300

    Добавил метод HandleUpdateAsync

--- VoiceTexterBot/Bot.cs
+++ VoiceTexterBot/Bot.cs
@@ -2,11 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 
 namespace VoiceTexterBot
 {
     internal class Bot
     {
+        private ITelegramBotClient _telegramClient;
+        public Bot(ITelegramBotClient telegramClient)
+        {
+            _telegramClient = telegramClient;
+        }
+        async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update,
+            CancellationToken cansellationToken)
+        {
+            if (update.Type == UpdateType.CallbackQuery)
+            {
+                await
+                    _telegramClient.SendTextMessageAsync(update.Message.Chat.Id, "Вы нажали кнопку",
+                    cancellationToken: cansellationToken);
+                return;
+            }
+            if (update.Type == UpdateType.Message)
+            {
+                await
+                    _telegramClient.SendTextMessageAsync(update.Message.Chat.Id,
+                    "Вы отправили сообщение", cancellationToken: cansellationToken);
+                return;
+            }
+        }
     }
 }
