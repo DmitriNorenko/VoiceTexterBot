e75c1322d857530297cc22371f27c4a9d4412ab9
Name <email@exaple.ru>
2023-08-07 09:59:28 +0300

    DirectoryExtension

---
+++ VoiceTexterBot/Extensions/DirectoryExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoiceTexterBot.Extensions
+{
+    internal class DirectoryExtension
+    {
+        public static string GetSolutionRoot()
+        {
+            var dir = Path.GetDirectoryName(Directory.GetCurrentDirectory());
+            var fullName = Directory.GetParent(dir).FullName;
+            var projectRoot = fullName.Substring(0, fullName.Length - 4);
+            return Directory.GetParent(projectRoot)?.FullName;
+        }
+    }
+}
